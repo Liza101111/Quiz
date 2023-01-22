adcea993efbd9207c62877d7d1af3a6d3e70d787
Liza <lixia101111@gmail.com>
2023-01-23 00:51:02 +0200

    A simple class that contains a status field of the boolean type, will be able to fill in the details of this wrong operation in the message field

---
+++ src/main/java/com/project/quiz/dto/HealthcheckDto.java
@@ -0,0 +1,20 @@
+package com.project.quiz.dto;
+
+public class HealthcheckDto {
+
+    private boolean status;
+    private String message;
+
+    public HealthcheckDto(boolean status, String message){
+        this.status = status;
+        this.message = message;
+    }
+
+    public boolean isStatus(){
+        return status;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
