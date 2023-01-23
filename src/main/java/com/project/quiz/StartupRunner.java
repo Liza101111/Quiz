d42f5ced297f726fe7cbadbe2444a20f2f6805fe
Liza <lixia101111@gmail.com>
2023-01-23 13:43:19 +0200

    Created a PlayerEntity object

---
+++ src/main/java/com/project/quiz/StartupRunner.java
@@ -0,0 +1,18 @@
+package com.project.quiz;
+
+import com.project.quiz.entities.PlayerEntity;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log
+public class StartupRunner implements CommandLineRunner {
+    @Override
+    public void run(String...args) throws Exception{
+        log.info("Executing startup actions...");
+
+        PlayerEntity player = new PlayerEntity("John");
+        log.info("Created player: " + player);
+    }
+}
