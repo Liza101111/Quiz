3526f8dae3851ab8dc24fbd78d4eccfe6872db0a
Liza <lixia101111@gmail.com>
2023-01-23 15:53:30 +0200

    Saving the object in the database

--- src/main/java/com/project/quiz/StartupRunner.java
+++ src/main/java/com/project/quiz/StartupRunner.java
@@ -1,18 +1,33 @@
 package com.project.quiz;
 
 import com.project.quiz.entities.PlayerEntity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+
 
 @Component
 @Log
 public class StartupRunner implements CommandLineRunner {
+
+    @Autowired
+    private EntityManager entityManager;
+
     @Override
+    @Transactional
     public void run(String...args) throws Exception{
+
         log.info("Executing startup actions...");
 
         PlayerEntity player = new PlayerEntity("John");
         log.info("Created player: " + player);
+
+        entityManager.persist(player);
+        log.info("Same player after persist: " + player);
     }
 }
