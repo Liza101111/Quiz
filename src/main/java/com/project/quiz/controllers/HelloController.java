db55a4d4fe6f80370d96ac2a76be4953122742e6
Liza <lixia101111@gmail.com>
2023-01-23 13:18:34 +0200

    Added Thymeleaf and HelloController

---
+++ src/main/java/com/project/quiz/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.project.quiz.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(Model model) {
+        model.addAttribute("message", "Welcome,have a nice day!");
+        return "hello";
+    }
+
+}
