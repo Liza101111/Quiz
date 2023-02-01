7bc06c61d07fe182e6753cec6090cc03896bd486
Liza <lixia101111@gmail.com>
2023-02-01 18:38:39 +0200

    ass the POST method support for the /select page

--- src/main/java/com/project/quiz/frontend/FrontendController.java
+++ src/main/java/com/project/quiz/frontend/FrontendController.java
@@ -1,11 +1,15 @@
 package com.project.quiz.frontend;
 
 
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Log
 public class FrontendController {
 
     @GetMapping("/")
@@ -18,6 +22,12 @@ public class FrontendController {
     public String select(Model model){
         model.addAttribute("gameOptions", new GameOptions());
         return "select";
+    }
+
+    @PostMapping("/select")
+    public String postSelectForm(Model model, @ModelAttribute GameOptions gameOptions) {
+        log.info("Form submitted with data: " + gameOptions);
+        return "index";
     }
 
 }
