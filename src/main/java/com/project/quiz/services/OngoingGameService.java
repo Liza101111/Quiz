73e6c157dcb330fe70e359fd8f713a62c4e7f5e0
Liza <lixia101111@gmail.com>
2023-02-03 17:26:43 +0200

    Add more methods in OngoingGameService to successfully create a game screen

--- src/main/java/com/project/quiz/services/OngoingGameService.java
+++ src/main/java/com/project/quiz/services/OngoingGameService.java
@@ -2,10 +2,15 @@ package com.project.quiz.services;
 
 import com.project.quiz.dto.QuestionsDto;
 import com.project.quiz.frontend.GameOptions;
+import lombok.Getter;
 import lombok.extern.java.Log;
+import org.hibernate.sql.ast.tree.expression.Collation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -13,6 +18,7 @@ import java.util.List;
 public class OngoingGameService {
     private GameOptions gameOptions;
     private int currentQuestionIndex;
+    @Getter
     private int points;
 
     private List<QuestionsDto.QuestionDto> questions;
@@ -25,6 +31,63 @@ public class OngoingGameService {
         this.currentQuestionIndex = 0;
         this.points = 0;
         this.questions = quizDataService.getQuizQuestions(gameOptions);
-
     }
+
+    public int getCurrentQuestionNumber(){
+        return currentQuestionIndex+1;
+    }
+
+    public int getTotalQuestionNumber(){
+        return questions.size();
+    }
+
+    public String getCurrentQuestion(){
+        QuestionsDto.QuestionDto dto = questions.get(currentQuestionIndex);
+        return dto.getQuestion();
+    }
+
+    public List<String> getCurrentQuestionAnswersInRandomOrder(){
+        QuestionsDto.QuestionDto dto = questions.get(currentQuestionIndex);
+
+        List<String> answers = new ArrayList<>();
+        answers.add(dto.getCorrectAnswer());
+        answers.addAll(dto.getIncorrectAnswers());
+
+        Collections.shuffle(answers);
+        return answers;
+    }
+
+    public boolean checkAnswerForCurrentQuestionAndUpdatePoints(String userAnswer) {
+        QuestionsDto.QuestionDto dto = questions.get(currentQuestionIndex);
+        boolean correct = dto.getCorrectAnswer().equals(userAnswer);
+        if(correct) {
+            points++;
+        }
+        return correct;
+    }
+
+    public boolean proceedToNextQuestion() {
+        currentQuestionIndex++;
+        return currentQuestionIndex < questions.size();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
